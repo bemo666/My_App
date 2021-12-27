09c591a383efd0bf3e5c3a6086105b329d8b42e7
Ben <poepnlpoep@gmail.com>
2021-12-27 17:36:30 +0200

    Added Birthday Field in SignUp activity

--- app/src/main/java/com/ikea/myapp/SignUp.java
+++ app/src/main/java/com/ikea/myapp/SignUp.java
@@ -2,15 +2,44 @@ package com.ikea.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
-public class SignUp extends AppCompatActivity {
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
+public class SignUp extends AppCompatActivity {
+    TextInputEditText birthday;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         getSupportActionBar().hide();
+
+        birthday = findViewById(R.id.BirthdayField);
+
+        DatePickerDialog datePicker = new DatePickerDialog(this);
+
+        birthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePicker.show();
+            }
+        });
+
+        datePicker.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                i1 += 1;
+                String date = i2 + "/" + i1 + "/" + i;
+                birthday.setText(date);
+
+            }
+        });
+
     }
 }
